0b7bb08a8d09d0a7003d973e31840308739f97ad
Eduardo Mulhia <emulhiad@gmail.com>
2024-03-20 16:51:32 -0600

    Seeder artistas

--- segundoParcial/database/seeders/ArtistSeeder.php
+++ segundoParcial/database/seeders/ArtistSeeder.php
@@ -4,14 +4,35 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Artists;
 
 class ArtistSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     * 
+     * @return void
      */
     public function run(): void
     {
-        //
+        $artist = new Artists();
+        $artist -> artist_name = "Hugo";
+        $artist -> save();
+
+        $artist = new Artists();
+        $artist -> artist_name = "Paco";
+        $artist -> save();
+
+        $artist = new Artists();
+        $artist -> artist_name = "Luis";
+        $artist -> save();
+
+        $artist = new Artists();
+        $artist -> artist_name = "Manolo";
+        $artist -> save();
+
+        $artist = new Artists();
+        $artist -> artist_name = "Tomate";
+        $artist -> save();
     }
 }
