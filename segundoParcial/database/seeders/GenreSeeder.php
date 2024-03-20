fa25991b2631d4fa42ae1d26a9b363c221564919
Eduardo Mulhia <emulhiad@gmail.com>
2024-03-20 16:49:21 -0600

    Seeder generos

--- segundoParcial/database/seeders/GenreSeeder.php
+++ segundoParcial/database/seeders/GenreSeeder.php
@@ -4,14 +4,35 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Genres;
 
 class GenreSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     * 
+     * @return void
      */
     public function run(): void
     {
-        //
+        $genre = new Genres();
+        $genre -> genre_name = "Pop";
+        $genre -> save();
+
+        $genre = new Genres();
+        $genre -> genre_name = "Rock";
+        $genre -> save();
+
+        $genre = new Genres();
+        $genre -> genre_name = "Opera";
+        $genre -> save();
+
+        $genre = new Genres();
+        $genre -> genre_name = "Blues";
+        $genre -> save();
+
+        $genre = new Genres();
+        $genre -> genre_name = "Jazz";
+        $genre -> save();
     }
 }
